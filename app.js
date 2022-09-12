20a2ab6e2aa0a3028e570027af7eb86ca752d11e
kpiv20 <kpivaral20@gmail.com>
2022-09-12 15:18:19 -0400

    added icon element and setattribute to match weather description w/ icon added "src" and removed "alt text" for the weather description code

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 function formatDate(timestamp){
     let date= new Date(timestamp);
     let hours= date.getHours();
@@ -19,6 +20,7 @@ let descriptionElement=document.querySelector("#description");
 let humidityElement=document.querySelector("#humidity");
 let windElement=document.querySelector("#wind");
 let dateElement=document.querySelector("#date");
+let iconElement=document.querySelector("#icon")
 
 temperatureElement.innerHTML=Math.round(response.data.main.temp);
 cityElement.innerHTML=response.data.name;
@@ -26,10 +28,11 @@ descriptionElement.innerHTML=response.data.weather[0].description;
 humidityElement.innerHTML=response.data.main.humidity;
 windElement.innerHTML=Math.round(response.data.wind.speed);
 dateElement.innerHTML=formatDate(response.data.dt*1000);
+iconElement.setAttribute ("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 let apiKey= "b5fb4a526e24f0e48b27c52886b74e1a"
-let city= "Atlanta"
+let city= "Anchorage"
 let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
 axios.get(apiUrl).then(displayTemperature);
